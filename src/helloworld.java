3949ce3344d34b0014c7c626b70bcabd94417356
jjz20200313 <18756051140@163.com>
2022-06-18 11:52:24 +0800

    Initial commit

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+
+public class helloworld {
+
+    public static void  main(String[] args){
+
+        String des="this is my first try of github project.";
+        System.out.println("hello world!");
+        System.out.println(des);
+
+    }
+
+}
